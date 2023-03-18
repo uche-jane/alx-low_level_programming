8ddd304bdc7737f8bb043966fc71df9dff5fa0ee
uche-jane <slimjoy07@gmail.com>
2023-03-17 20:00:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return : Always 0 (success)
+ */
+int main(void) 
+{
+printf("On a canvas of black, splattered with hues of red,\n"
+         "A masterpiece emerges, a sight to behold!\n"
+         "Each stroke, a story told,\n"
+         "Every color, a feeling said.\n"
+         "As the painting dries, it comes to life,\n"
+         "A creation unique, an expression of strife.\n"
+         "In this artwork, the artist pours their heart,\n"
+         "A masterpiece of emotions, a true work of art.\n");
+return(0);
+}
