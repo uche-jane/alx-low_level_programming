fbec5d3bd695c98cde817b47063c12bf37fec92c
uche-jane <slimjoy07@gmail.com>
2023-03-20 19:43:08 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,29 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-Here's a possible implementation of the requested program:
 
-c
-Copy code
 int main(void)
 {
-    int d1, d2, d3;
+    int i, j, k;
 
-    for (d1 = 0; d1 < 8; d1++) {
-        for (d2 = d1 + 1; d2 < 9; d2++) {
-            for (d3 = d2 + 1; d3 < 10; d3++) {
-                putchar(d1 + '0');
-                putchar(d2 + '0');
-                putchar(d3 + '0');
+    for (i = 0; i < 8; i++)
+    {
+        for (j = i + 1; j < 9; j++)
+        {
+            for (k = j + 1; k < 10; k++)
+            {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
 
-                if (d1 == 7 && d2 == 8 && d3 == 9) {
-                    putchar('\n');
-                } else {
+                if (i != 7 || j != 8 || k != 9)
+                {
                     putchar(',');
                     putchar(' ');
                 }
             }
         }
     }
+
+    putchar('\n');
 
     return 0;
 }
