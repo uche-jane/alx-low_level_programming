2e869f6e72eaace552f3f40b29af91c72c472c54
uche-jane <slimjoy07@gmail.com>
2023-03-21 20:16:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* print_ alphabet - print all alphabet in lower case
+* Return: 0
+*/
+
+void print_alphabet(void);
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++);
+	_putchar(letter);
+	_putchar('\n')
+   
+        {
