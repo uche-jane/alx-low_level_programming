34b104889438f75c166f9f1d98840a2a55929285
uche-jane <slimjoy07@gmail.com>
2023-03-22 21:12:41 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+    if (n <= 98) {
+        for (int i = n; i <= 98; i++) {
+            printf("%d", i);
+            if (i < 98) {
+                printf(", ");
+            }
+        }
+    } else {
+        for (int i = n; i >= 98; i--) {
+            printf("%d", i);
+            if (i > 98) {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
+}
+
