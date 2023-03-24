a2fd88ddcb8a77424741aca64661289f6c017854
uche-jane <slimjoy07@gmail.com>
2023-03-23 20:56:29 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *
+ *_putchar writes the character to standout
+ *
+ * Return on success is 1
+ *
+ *
+ *
+ *
+ */
+int _putchar(char c)
+{
+	return(0)
+}
