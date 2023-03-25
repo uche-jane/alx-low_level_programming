667733687031947f1e9069b3cd190dbef1467515
uche-jane <slimjoy07@gmail.com>
2023-03-24 19:09:07 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,12 +3,12 @@
 
 void print_most_numbers(void) 
 {
-    int i;
-    for(i = 0; i < 10; i++) 
+    int c;
+    for(c = 0; c <= 9; c++) 
     {
-        if(i != 2 && i != 4) 
+        if(c != 2 && c != 4) 
 	{
-            _putchar(i + '0');
+            _putchar(c + '0');
         }
     }
     _putchar('\n');
