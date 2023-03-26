f1031735bc28e9fd4a77355ca5c46a2f2cdd446c
uche-jane <slimjoy07@gmail.com>
2023-03-26 14:07:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -6,3 +6,13 @@
  * Return: Always 0.
  */
 int main(void)
+{
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
+}
