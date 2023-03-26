516da563e1e2b2d44bd5ae64068a01b09d8cdd70
uche-jane <slimjoy07@gmail.com>
2023-03-26 13:37:16 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-void print_to_98(int n)
-{
+void print_to_98(int n) {
+    int i;
     if (n <= 98) {
-        for (int i = n; i <= 98; i++) {
-            printf("%d", i);
-            if (i < 98) {
-                printf(", ");
-            }
+        for (i = n; i < 98; i++) {
+            printf("%d, ", i);
         }
     } else {
-        for (int i = n; i >= 98; i--) {
-            printf("%d", i);
-            if (i > 98) {
-                printf(", ");
-            }
+        for (i = n; i > 98; i--) {
+            printf("%d, ", i);
         }
     }
-    printf("\n");
+    printf("98\n");  /* Print the last number followed by a newline character*/
 }
 
