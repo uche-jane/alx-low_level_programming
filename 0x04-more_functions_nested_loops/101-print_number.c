22910dc764f643adc7c9b331dc9ce5746ed181ab
uche-jane <slimjoy07@gmail.com>
2023-03-26 05:53:01 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,20 @@
  */
 void print_number(int n)
 {
+	unsigned int n1;
     if (n < 0)
     {
         _putchar('-');
-        n = -n;
+        n1 = -n;
     }
-
+    else
+    {
+	    n1 = n;
     if (n / 10)
     {
         print_number(n / 10);
     }
 
-    _putchar(n % 10 + '0');
+    _putchar((n1 % 10) + '0');
 }
 
