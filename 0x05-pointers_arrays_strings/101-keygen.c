d412373485419833f10f79c0483c147de08b256f
uche-jane <slimjoy07@gmail.com>
2023-03-27 14:47:57 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LEN 6
+#define PASSWORD_LEN 12
 
 int main(void)
 {
-    char password[PASSWORD_LEN + 1]; // +1 for null terminator
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    char password[PASSWORD_LEN + 1]; /* +1 for null terminator */
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]\\|:;\"'<>,.?/";
     const int charset_len = sizeof(charset) - 1;
+    int i;
 
-    srand(time(NULL)); /* initialize random number generator with current time*/
+    srand(time(NULL)); /* initialize random number generator with current time */
 
-    for (int i = 0; i < PASSWORD_LEN; i++) {
+    for (i = 0; i < PASSWORD_LEN; i++) {
         password[i] = charset[rand() % charset_len];
     }
 
-    password[PASSWORD_LEN] = '\0'; /* add null terminator*/
+    password[PASSWORD_LEN] = '\0'; /* add null terminator */
 
     printf("%s\n", password);
 
