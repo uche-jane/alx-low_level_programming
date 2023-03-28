9711e5fd6f02973ebf3a2f003b0a640799ebaa9a
uche-jane <slimjoy07@gmail.com>
2023-03-28 09:35:28 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,27 @@
 #include "main.h"
+#include <string.h>
 
-char*_strcpy(char*dest, char*src) 
+#include <stdlib.h>
+/**
+ *
+ *char *_strcpy
+ *
+ *@*dest
+ *@*src
+ *
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
 {
-    char src[] = "hello world";
-    char dest[20];
-    char *result = copy_string(dest, src);
-    printf("Source string: %s\n", src);
-    printf("Destination string: %s\n", dest);
-    printf("Return value: %s\n", result);
-    return 0;
+    char *p = dest;
+    while (*src != '\0') {
+        *p++ = *src++;
+    }
+    *p = '\0';
+    return dest;
 }
+
+
 
 
