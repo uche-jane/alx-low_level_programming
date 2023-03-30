a0f7042b4d272a07da234f87b0042329c616aa31
uche-jane <slimjoy07@gmail.com>
2023-03-30 07:12:08 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,9 +16,9 @@ int main(void) {
     /* Add the following line*/
     printf("a[2] = %d\n", *(p + 2));
 
-    printf("a[0] = %d\n", *p);
-    printf("a[1] = %d\n", *(p + 1));
-    printf("a[2] = %d\n", *(p + 2));
+   /** printf("a[0] = %d\n", *p);
+   * printf("a[1] = %d\n", *(p + 1));
+   */ printf("a[2] = %d\n", *(p + 2));
 
     return 0;
 }
