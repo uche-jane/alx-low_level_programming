8c7adf29efcc5ce876ef38c6838a01612fb8c9d1
uche-jane <slimjoy07@gmail.com>
2023-03-30 07:06:53 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,13 +6,21 @@
  * Return: Always 0.
  */
 
-int main() 
-{
-    int a[] = {87, 92, 98, 120, 77, 82};
-    int* p = a;
+#include <stdio.h>
 
-    printf("a[2] = %d\n", *(p+2)); /* added line*/
-    
+int main(void) {
+    int a[] = {99, 98, 97};
+    int *p;
+    p = &a[0];
+
+    /* Add the following line*/
+    printf("a[2] = %d\n", *(p + 2));
+
+    printf("a[0] = %d\n", *p);
+    printf("a[1] = %d\n", *(p + 1));
+    printf("a[2] = %d\n", *(p + 2));
+
     return 0;
 }
+
 
