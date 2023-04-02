c3ba1272fb575a586b35eea5012f2216b541d8a1
uche-jane <slimjoy07@gmail.com>
2023-04-01 22:30:30 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-char *cap_string(char *str) {
-  int i;
-  char *result = malloc(strlen(str) + 1);
-  char *word;
-  const char *delim = " \t\n,;.!?\"(){}";
-  
-  strcpy(result, str);
-  word = strtok(result, delim);
-  
-  while (word != NULL) {
-    for (i = 0; word[i] != '\0'; i++) {
-      word[i] = toupper(word[i]);
-    }
-    
-    word = strtok(NULL, delim);
-  }
-  
-  return result;
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
 }
+
