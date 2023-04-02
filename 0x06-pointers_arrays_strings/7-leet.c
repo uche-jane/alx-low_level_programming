22e705a4834b9bbcf63a244dcc04176bd699de8a
uche-jane <slimjoy07@gmail.com>
2023-04-01 22:33:05 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *leet(char *str) {
-  int i, j;
-  char *result = malloc(strlen(str) + 1);
-  
-  for (i = 0, j = 0; str[i] != '\0'; i++, j++) {
-    if (str[i] == 'a' || str[i] == 'A') {
-      result[j] = '4';
-    } else if (str[i] == 'e' || str[i] == 'E') {
-      result[j] = '3';
-    } else if (str[i] == 'o' || str[i] == 'O') {
-      result[j] = '0';
-    } else if (str[i] == 't' || str[i] == 'T') {
-      result[j] = '7';
-    } else if (str[i] == 'l' || str[i] == 'L') {
-      result[j] = '1';
-    } else {
-      result[j] = str[i];
-    }
-  }
-  
-  result[j] = '\0';
-  return result;
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ *
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
 }
